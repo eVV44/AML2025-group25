0df97f8d912c9b23502efca5b5d273c49ac91e2a
eVV44 <118945363+eVV44@users.noreply.github.com>
2025-12-05 12:11:55 +0100

    BirdDataset and data loading

--- dataset_util.py
+++ dataset_util.py
@@ -1,4 +1,32 @@
+# -- IMPORTS --
 from torchvision import transforms
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
 
 # dataset augmentation, loading, splitting, transform to uniform size
 # maybe use torch dataset and dataloader?
+
+class BirdDataset(Dataset):
+    """
+    Dataset tranformation.
+    """
+    def __init__(self, df, transform=None):
+        self.df = df.reset_index(drop=True)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+    
+        img_path = row["image_path"]
+        label = row["label_idx"]
+
+        image = Image.open(img_path).convert("RGB")
+
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        return image, label
