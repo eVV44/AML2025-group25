9be3a33b12ccdcf7c3fa8624a8ebab892061a4ca
JorK <joris.kingmans@gmail.com>
2025-12-05 16:52:33 +0100

    simple test set prediction

--- dataset_util.py
+++ dataset_util.py
@@ -3,6 +3,9 @@ from torchvision import transforms
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
+import pandas as pd
+from typing import Tuple
+from datetime import datetime
 
 # dataset augmentation, loading, splitting, transform to uniform size
 # maybe use torch dataset and dataloader?
@@ -30,3 +33,40 @@ class BirdDataset(Dataset):
             image = self.transform(image)
         
         return image, label
+    
+
+class TestSet():
+    def __init__(self, img_transform_size: Tuple[int] = (224,224)):
+        self.test_df = pd.read_csv('data/test_images_path.csv')
+        self.test_df['image_path'] = 'data/test_images' + self.test_df['image_path']
+        self.transform = transforms.Compose(
+            [
+                transforms.Resize(img_transform_size),
+                transforms.ToTensor()
+            ]
+        )
+
+    def predict(self, img_path, model):
+        model.eval()
+
+        img = Image.open(img_path).convert('RGB')
+        img = self.transform(img)
+        img = img.unsqueeze(0)
+
+        with torch.no_grad():
+            logits = model(img)
+            pred = logits.argmax(dim=1).item()
+        
+        return pred
+
+    def predict_all(self, prediction_model):
+        self.test_df['label'] = self.test_df['image_path'].apply(self.predict, model=prediction_model)
+
+    def get_df(self):
+        return self.test_df
+    
+    def convert_to_submission(self):
+        submission = self.test_df.copy(deep=True)
+        submission.drop(columns=['image_path'], inplace=True)
+        now_str = datetime.now().strftime("%d-%m-%Y_at_%H_%M") 
+        submission.to_csv(f'predictions/predictions_{now_str}.csv', index=False)
